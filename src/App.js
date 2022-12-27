93423f865b99acfd377e7ead02934d130add8fb0
Joyin2 <joyinlaskar2000@gmail.com>
2022-12-28 00:47:40 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-import About from "./pages/About/About";
-import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import Contact from "./pages/Contact/Contact";
+import React from 'react'
+import Router from './routes/router'
+
 
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component ={Home}/>
-          <Route exact path="/About" component ={About}/>
-          <Route exact path="/Contact" component ={Contact}/>
-          <Redirect to="/" />
-        </Switch>
-      </Router>
+      <Router />
     </>
-  );
+  
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <>
-      
-    </>
+    <div>
+      Hello Home Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat necessitatibus quod et aliquam optio blanditiis natus, nobis nihil ut possimus tempora, sunt, aspernatur perspiciatis sequi saepe voluptates consequuntur dolorum quis molestiae a eveniet? Alias quam repudiandae, quos illum dolore quas. Deserunt velit deleniti repellendus accusamus quod odit id. Sunt, impedit?
+    </div>
   )
 }
 
